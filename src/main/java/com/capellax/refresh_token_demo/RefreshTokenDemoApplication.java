7f6739b5eaa2aa31e0b8bec14a49913b447b261a
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-05 11:04:38 +0300

    Spring Boot application started & PostgreSQL database connected

---
+++ src/main/java/com/capellax/refresh_token_demo/RefreshTokenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.capellax.refresh_token_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshTokenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefreshTokenDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capellax/refresh_token_demo/RefreshTokenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capellax.refresh_token_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshTokenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
