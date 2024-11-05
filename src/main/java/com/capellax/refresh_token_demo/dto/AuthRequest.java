3e1d4e5152eb908a4116d19e2b92a654063dae6c
CAPELLAX02 <ahmtatar@gmail.com>
2024-11-05 14:59:36 +0300

    response/request DTOs completed

---
+++ src/main/java/com/capellax/refresh_token_demo/dto/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.capellax.refresh_token_demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+}
